ebe5299689880ae454e618c3f9666f967c632b3c
Alexandr Golovach <alexmprog@gmail.com>
2023-07-12 08:57:13 +0300

    fix: add anonymous auth

--- gamification/src/main/java/io/streamlayer/demo/gamification/App.kt
+++ gamification/src/main/java/io/streamlayer/demo/gamification/App.kt
@@ -3,6 +3,9 @@ package io.streamlayer.demo.gamification
 import android.app.Application
 import io.streamlayer.sdk.StreamLayer
 import io.streamlayer.demo.common.exo.ExoVideoPlayerProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class App : Application() {
 
@@ -12,6 +15,8 @@ class App : Application() {
         const val DEMO_EVENT_ID = "demo"
     }
 
+    val appScope by lazy { CoroutineScope(Dispatchers.Default) }
+
     override fun onCreate() {
         super.onCreate()
         instance = this
@@ -19,5 +24,7 @@ class App : Application() {
         StreamLayer.initializeApp(this, SL_SDK_KEY)
         // set video player provider
         StreamLayer.setVideoPlayerProvider(ExoVideoPlayerProvider(this))
+        // authorize anonymous user
+        appScope.launch { StreamLayer.useAnonymousAuth() }
     }
 }
