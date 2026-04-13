e16d8f4cb0305efe8b71bb65fb8eff497fa7f88d
Ling Dam Tkhe <vivalatrunks@gmail.com>
2026-04-13 11:49:20 +0300

    update sdk, uuid

--- demotv/src/main/java/io/streamlayer/demotv/MainActivity.kt
+++ demotv/src/main/java/io/streamlayer/demotv/MainActivity.kt
@@ -2,6 +2,7 @@ package io.streamlayer.demotv
 
 import android.graphics.Outline
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,6 +31,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.UUID
 
 private const val TAG = "MainActivity"
 
@@ -143,7 +145,7 @@ class MainActivity : AppCompatActivity() {
                             webView {
                                 host = "https://bell-ad.streamlayer.io/"
                                 url =
-                                    "https://securepubads.g.doubleclick.net/gampad/ads?ad_rule=0&an=tsn&correlator=1774963552&description_url=https://www.tsn.ca/&env=instream&gdfp_req=1&hl=en&idtype=tifa&is_lat=true&iu=/5479/tsn_tsndigital_cotv_samsungtv/playerpage/na_nba-on-tsn&cust_params=envr%3Ddev%26contentrating%3Dagvote%2Cqfrg%26genre%3Dbasketball%26adtarget%3D%26subtype%3Dtsn%2Ctsn2%2Ctsn2_bdu%2Ctsn_bdu%26pagetitle%3Dnba-on-tsn-mavericks-vs-rockets%26mediatype%3Dsports%26contenttype%3Dgame%26revshare%3Dna%26lang%3Den%26adrule%3D&msid=ca.tsn.tsngo&output=ldjh&pp=app_profile&rdid=&sz=315x400&tfcd=0&unviewed_position_start=1&url=ca.tsn.tsngo&vid=8243165&sid=a3098bb7-ae67-4d8c-b4be-3309cf64ca0d&impl=s&video_doc_id=8243165&scor=879066875&npa=0&wta=1&plcmt=1"
+                                    "https://securepubads.g.doubleclick.net/gampad/ads?ad_rule=0&an=tsn&correlator=1774963552&description_url=https://www.tsn.ca/&env=instream&gdfp_req=1&hl=en&idtype=adid&is_lat=true&iu=/5479/tsn_tsndigital_cotv_samsungtv/playerpage/na_nba-on-tsn&cust_params=envr%3Ddev%26contentrating%3Dagvote%2Cqfrg%26genre%3Dbasketball%26adtarget%3D%26subtype%3Dtsn%2Ctsn2%2Ctsn2_bdu%2Ctsn_bdu%26pagetitle%3Dnba-on-tsn-mavericks-vs-rockets%26mediatype%3Dsports%26contenttype%3Dgame%26revshare%3Dna%26lang%3Den%26adrule%3D&msid=ca.tsn.tsngo&output=ldjh&pp=app_profile&rdid=${loadAndroidId()}&sz=315x400&tfcd=0&unviewed_position_start=1&url=ca.tsn.tsngo&vid=8243165&sid=a3098bb7-ae67-4d8c-b4be-3309cf64ca0d&impl=s&video_doc_id=8243165&scor=879066875&npa=0&wta=1&plcmt=1"
                             }
                         }
                     }
@@ -167,6 +169,15 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    //Generate UUID of device
+    private fun loadAndroidId(): String? = try {
+        val androidId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        UUID.nameUUIDFromBytes(androidId.toByteArray()).toString().also {
+        }
+    } catch (e: Exception) {
+        null
     }
 
     // load demo streams and select first
